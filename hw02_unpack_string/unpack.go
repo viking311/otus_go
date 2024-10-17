c0f83d19e5590396e9fc10874a6f03b3cabb331f
Viktor Rikhert <viking311@gmail.com>
2024-10-17 17:20:38 +0700

    implement first iteration

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,64 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	runes := []rune(input)
+
+	var result strings.Builder
+	var prevRune rune
+	var escape bool
+	var i int
+
+	for i < len(runes) {
+		if unicode.IsLetter(runes[i]) {
+			if escape {
+				return "", ErrInvalidString
+			}
+			if prevRune == 0 {
+				prevRune = runes[i]
+			} else {
+				result.WriteRune(prevRune)
+				prevRune = runes[i]
+			}
+			i++
+		} else if unicode.IsDigit(runes[i]) {
+			if prevRune == 0 && !escape {
+				return "", ErrInvalidString
+			}
+			if escape {
+				result.WriteRune(prevRune)
+				prevRune = runes[i]
+				escape = false
+			} else {
+				repCount, _ := strconv.Atoi(string(runes[i]))
+				repeatedStr := strings.Repeat(string(prevRune), repCount)
+				result.WriteString(repeatedStr)
+				prevRune = 0
+			}
+			i++
+		} else if runes[i] == '\\' {
+			if escape {
+				escape = false
+				result.WriteRune(prevRune)
+				prevRune = '\\'
+			} else {
+				escape = true
+			}
+			i++
+		} else {
+			return "", ErrInvalidString
+		}
+	}
+	if prevRune != 0 {
+		result.WriteRune(prevRune)
+	}
+
+	return result.String(), nil
 }
