64779e649464a2bc58d7931fff888e295a100ba4
Viktor Rikhert <viking311@gmail.com>
2024-11-14 12:46:37 +0700

    first iteration

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	nextIn := in
+	var nextOut Bi
+
+	for _, st := range stages {
+		nextOut = make(Bi)
+
+		go func(innerIn In, innerOut Bi) {
+			defer close(innerOut)
+			out := st(innerIn)
+			for {
+				select {
+				case <-done:
+					return
+				default:
+					select {
+					case <-done:
+						return
+					case el, ok := <-out:
+						if !ok {
+							return
+						}
+						innerOut <- el
+					}
+				}
+			}
+		}(nextIn, nextOut)
+
+		nextIn = nextOut
+	}
+
+	return nextOut
 }
