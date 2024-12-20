2e6c74c418cc39c8450b5a25864773542e9b50ed
Viktor Rikhert <viking311@gmail.com>
2024-12-20 10:14:33 +0700

    implement copy tool

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,74 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []struct {
+		name   string
+		from   string
+		to     string
+		offset int64
+		limit  int64
+	}{
+		{
+			name:   "Empty from",
+			from:   "",
+			to:     "out.txt",
+			offset: 0,
+			limit:  0,
+		},
+		{
+			name:   "Empty to",
+			from:   "testdata/input.txt",
+			to:     "",
+			offset: 0,
+			limit:  0,
+		},
+		{
+			name:   "Negative offset",
+			from:   "testdata/input.txt",
+			to:     "out.txt",
+			offset: -10,
+			limit:  0,
+		},
+		{
+			name:   "Negative limit",
+			from:   "testdata/input.txt",
+			to:     "out.txt",
+			offset: 0,
+			limit:  -1,
+		},
+		{
+			name:   "From file isn't exists",
+			from:   "filename",
+			to:     "out.txt",
+			offset: 0,
+			limit:  0,
+		},
+		{
+			name:   "Unsuported file",
+			from:   "/dev/urandom",
+			to:     "out.txt",
+			offset: 0,
+			limit:  0,
+		},
+		{
+			name:   "ErrUnsupportedFile",
+			from:   "testdata/input.txt",
+			to:     "out.txt",
+			offset: 1000000,
+			limit:  0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Copy(tt.from, tt.to, tt.offset, tt.limit)
+			require.Error(t, err)
+		})
+	}
 }
