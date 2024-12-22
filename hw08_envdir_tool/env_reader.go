159893f3c6f75e963f02b682190ce945b93bd379
Viktor Rikhert <viking311@gmail.com>
2024-12-22 21:55:23 +0700

    implement base logic

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"log"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +16,78 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+var terminalZero, lineBreak []byte = []byte{0}, []byte{10}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	dirItems, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	env := make(Environment)
+
+	for _, dirItem := range dirItems {
+		fileInfo, err := dirItem.Info()
+		if err != nil {
+			return nil, err
+		}
+
+		if fileInfo.IsDir() {
+			continue
+		}
+
+		if strings.Contains(fileInfo.Name(), "=") {
+			continue
+		}
+
+		if fileInfo.Size() == 0 {
+			env[fileInfo.Name()] = EnvValue{NeedRemove: true}
+		}
+
+		value, err := readFile(dir + "/" + fileInfo.Name())
+		if err != nil {
+			return nil, err
+		}
+
+		if len(value) == 0 {
+			env[fileInfo.Name()] = EnvValue{NeedRemove: true}
+		} else {
+			env[fileInfo.Name()] = EnvValue{Value: value}
+		}
+	}
+
+	return env, nil
+}
+
+func readFile(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	defer func() {
+		errC := file.Close()
+		if errC != nil {
+			log.Println(errC)
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+	if !scanner.Scan() {
+		return "", nil
+	}
+	str := scanner.Text()
+
+	return trim(str), nil
+}
+
+func trim(content string) string {
+	strBytes := []byte(content)
+	strBytes = bytes.ReplaceAll(strBytes, terminalZero, lineBreak)
+	content = string(strBytes)
+	content = strings.TrimRight(content, "\t ")
+
+	return content
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+	if len(args) < 3 {
+		log.Fatalf("too few arguments")
+	}
+
+	env, err := ReadDir(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	returnCode := RunCmd(args[2:], env)
+
+	os.Exit(returnCode)
 }
