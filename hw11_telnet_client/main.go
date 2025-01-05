31f326ccee1694abe224f64a79eb2e3451a9fad9
Viktor Rikhert <viking311@gmail.com>
2025-01-05 18:01:21 +0700

    v1 implementation

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,55 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
+const defaultTimeout = 10
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", defaultTimeout*time.Second, "timeout connection default 10s")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 2 {
+		log.Fatal("Port or host not specified")
+	}
+
+	host := args[0]
+	port := args[1]
+
+	client := NewTelnetClient(net.JoinHostPort(host, port), *timeout, os.Stdin, os.Stdout)
+
+	err := client.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		errC := client.Close()
+		if errC != nil {
+			log.Println(errC)
+		}
+	}()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	go func() {
+		defer cancel()
+		if err := client.Send(); err != nil {
+			log.Fatalf("Error reading from channel. Error: %s", err)
+		}
+	}()
+
+	go func() {
+		defer cancel()
+		if err := client.Receive(); err != nil {
+			log.Fatalf("Error reading from channel. Error: %s", err)
+		}
+	}()
+	<-ctx.Done()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +15,60 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+type Telnet struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (tl *Telnet) Connect() error {
+	var err error
+
+	tl.conn, err = net.DialTimeout("tcp", tl.address, tl.timeout)
+
+	return err
+}
+
+func (tl *Telnet) Close() error {
+	if tl.conn == nil {
+		return fmt.Errorf("connection is not open")
+	}
+
+	return tl.conn.Close()
+}
+
+func (tl *Telnet) Send() error {
+	scanner := bufio.NewScanner(tl.in)
+
+	for scanner.Scan() {
+		text := scanner.Text() + "\n"
+		_, err := tl.conn.Write([]byte(text))
+		if err != nil {
+			return err
+		}
+	}
+
+	return scanner.Err()
+}
+
+func (tl *Telnet) Receive() error {
+	scanner := bufio.NewScanner(tl.conn)
+	for scanner.Scan() {
+		_, err := fmt.Fprintln(tl.out, scanner.Text())
+		if err != nil {
+			return err
+		}
+	}
+	return scanner.Err()
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &Telnet{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
